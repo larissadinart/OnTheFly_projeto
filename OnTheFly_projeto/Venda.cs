ed6d0f3e53c9d67aa8c75f8c2feeea251f61b7b9
Larissa Dinart <lari_dinart@hotmail.com>
2022-09-13 09:30:56 -0300

    implementação do metodo LocalizarVoo

--- OnTheFly_projeto/Venda.cs
+++ OnTheFly_projeto/Venda.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace OnTheFly_projeto
 {
-    
+
     internal class Venda
     {
-        public int Id { get; set; } // CHAVE!! Deverá ser um número sequencial que iniciará em 1 e, através deste sistema poderá ir apenas até a venda 99.999. Sendo assim, será numérico, mas deverá ter no máximo 5 dígitos.
+        private int id;
+
+        public int Id { get; set; } // CHAVE!!
         public DateTime DataVenda { get; set; }
         public String Passageiro { get; set; } //Deverá armazenar apenas o CPF do Passageiro, mas ao ser informado, deverá trazer o nome e a data de nascimento do cliente, para uma verificação junto ao mesmo.
         public int ValorTotal { get; set; } //Deverá armazenar apenas o CPF do Passageiro, mas ao ser informado, deverá trazer o nome e a data de nascimento do cliente, para uma verificação junto ao mesmo.
@@ -31,14 +33,14 @@ namespace OnTheFly_projeto
         {
             Venda venda = new Venda();
 
-            venda.Id = 1;
+            venda.Id = GeraNumero(id);
             venda.DataVenda = DateTime.Now;
 
             Console.WriteLine("Digite o CPF do passageiro: ");
             passageiro.Cpf = Console.ReadLine();
             foreach (var item in restritos)
             {
-                if(restritos.Equals(item))
+                if (restritos.Equals(item))
                 {
                     Console.WriteLine("CPF restrito!Venda não pode ser realizada!\n\nAperte enter para continuar...");
                     Console.ReadKey();
@@ -49,7 +51,7 @@ namespace OnTheFly_projeto
                 if (passageiro.Cpf == item.Cpf)
                 {
                     int op = 0;
-                    
+
                     Console.WriteLine(item.Nome);
                     Console.WriteLine(item.DataNascimento);
                     do
@@ -59,11 +61,13 @@ namespace OnTheFly_projeto
                         switch (op)
                         {
                             case 1:
+                                voo.LocalizarVoo(listaDeVoo);
                                 break;
-                            case 2:CadastrarVenda(passageiro,voo,passageiros,restritos);
+                            case 2:
+                                CadastrarVenda(passageiro, voo, passageiros, restritos);
                                 break;
                         }
-                    }while(op < 0 || op > 2);
+                    } while (op < 0 || op > 2);
                 }
                 else
                 {
@@ -72,15 +76,22 @@ namespace OnTheFly_projeto
                 }
             }
         }
-
-        internal void CadastrarVenda()
+        public int GeraNumero(int nro)
         {
-            throw new NotImplementedException();
+            int n = nro;
+            if (n > 99999)
+            {
+                n = 0;
+            }
+            else
+            {
+                n++;
+            }
+            return n;
         }
 
-        public void GerarNumeroSequencial() //gera numero sequencial iniciado em 1
-        {
-    
-        }
+
+
+
     }
 }
